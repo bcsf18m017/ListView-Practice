ba8b5483677b43840584ce91a6d8c644d900b2a0
Chintu <bcsf18m017@pucit.edu.pk>
2021-11-16 21:28:02 +0500

    Add Button

--- app/src/main/java/com/example/listviewpractice/MainActivity.java
+++ app/src/main/java/com/example/listviewpractice/MainActivity.java
@@ -3,30 +3,39 @@ package com.example.listviewpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView l;
-    String tutorials[]
-            = { "Mobile", "Computing",
-            "Android", "Studio",
-            "JAVA", "Gradle",
-            "Binder IPC", "HAL"};
-
+    ArrayList<String> data;
+    Button btn;
+    EditText txt;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        data=new ArrayList<>();
         l = findViewById(R.id.list);
+        btn=findViewById(R.id.addButton);
+        txt=findViewById(R.id.textValue);
         ArrayAdapter<String> arr;
-        arr
-                = new ArrayAdapter<String>(
-                this,
-                R.layout.support_simple_spinner_dropdown_item,
-                tutorials);
+        arr = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, data);
         l.setAdapter(arr);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                data.add(txt.getText().toString());
+                arr.notifyDataSetChanged();
+                txt.setText("");
+            }
+        });
     }
 }
